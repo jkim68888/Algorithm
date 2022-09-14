a0571d0686ec9ef111b1a9d6e3ee4819498b65b4
jkim68888 <jkim68888@gmail.com>
2022-09-14 17:05:05 +0900

    콜라츠 추측 풀이

---
+++ 프로그래머스/연습문제/Lv1/lessons12943.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// 나누어 떨어지는 숫자 배열
+// https://school.programmers.co.kr/learn/courses/30/lessons/12943
+
+
+// 나의 풀이
+func solution(_ num:Int) -> Int {
+	var temp: Int = num
+	var cnt: Int = 0
+	
+	while cnt < 501, temp != 1 {
+		if temp % 2 == 0 {
+			temp = temp / 2
+		} else {
+			temp = temp * 3 + 1
+		}
+		cnt += 1
+	}
+
+	return cnt == 501 ? -1 : cnt
+}
+
+solution(1)
+solution(6)
+solution(16)
+solution(626331)
